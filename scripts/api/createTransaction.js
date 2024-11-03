2d780c9d4ecee51ee9c757515735e125373ba45d
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-03 02:12:03 +0200

    Edit - include Id in local storage to keep track of unique ids for transaction

--- scripts/api/createTransaction.js
+++ scripts/api/createTransaction.js
@@ -2,33 +2,38 @@ const typeOfTransaction = {
   0: "Expense",
   1: "Income",
 };
-if(!localStorage.getItem("ID") )
-{
-    localStorage.setItem("ID".0)
+
+if (!localStorage.getItem("ID")) {
+  localStorage.setItem("ID", 0);
 }
-let id = 1;
 
 const createTransaction = () => {
-  //check if input filled
-  const transaction = {
-    id: id,
-    type: transactionType.value,
-    amount: transactionAmount.value,
-    date: transactionDate.value,
-    notes: transactionNotes.value,
-  };
-  id += 1;
+  let id = Number.parseInt(localStorage.getItem("ID"));
+  if (Number.isInteger(id)) {
+    //check if input filled
+    const transaction = {
+      id: id,
+      type: transactionType.value,
+      amount: transactionAmount.value,
+      date: transactionDate.value,
+      notes: transactionNotes.value,
+    };
 
-  localStorage.setItem(`${id}`, JSON.stringify(transaction));
-  window.location.reload();
+    localStorage.setItem(`${id}`, JSON.stringify(transaction));
+    id += 1;
+    localStorage.setItem("ID", id);
+    window.location.reload();
+  }
 };
 submitBtn.addEventListener("click", createTransaction);
 
 const getAllTransactions = () => {
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
-    const transaction = JSON.parse(localStorage.getItem(key));
-    displayTransaction(transaction);
+    if (key != "ID") {
+      const transaction = JSON.parse(localStorage.getItem(key));
+      displayTransaction(transaction);
+    }
   }
 };
 getAllTransactions();
